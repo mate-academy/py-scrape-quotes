3674763eb057bbd68ba1be78373ad92119e89262
Vasyl Smutok <23.smutok@gmail.com>
2022-08-30 23:51:15 +0300

    solution optional task

--- app/parse.py
+++ app/parse.py
@@ -6,7 +6,18 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 HOME_PAGE = "https://quotes.toscrape.com/"
+
 QUOTE_FIELDS = ["text", "author", "tags"]
+AUTHOR_FIELDS = ["full name", "place of born", "date of born", "descriptions"]
+AUTHORS_OUTPUT_CSV_PATH = "authors.csv"
+
+
+@dataclass
+class Author:
+    full_name: str
+    place_of_born: str
+    date_of_born: str
+    descriptions: str
 
 
 @dataclass
@@ -16,11 +27,40 @@ class Quote:
     tags: list[str]
 
 
+def get_detail_of_author(author_soup: BeautifulSoup) -> dict:
+    date_of_born = author_soup.select_one(".author-born-date").text
+    place_of_born = author_soup.select_one(".author-born-location").text
+    descriptions = author_soup.select_one(".author-description").text
+
+    return {
+        "date_of_born": date_of_born,
+        "place_of_born": place_of_born,
+        "descriptions": descriptions
+    }
+
+
+def parse_single_author(author) -> dict:
+    if author[-1] == ".":
+        author = author[:-1]
+
+    author = author.replace(". ", "-").replace(" ", "-").replace(".", "-").replace("é", "-").replace("'", "")
+
+    page = requests.get(f"{HOME_PAGE}/author/{author}/").content
+    page_soup = BeautifulSoup(page, "html.parser")
+    return get_detail_of_author(page_soup)
+
+
+all_authors = {}  # dictionary in which all information about the authors is stored
+
+
 def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
     text = quote_soup.select_one(".text").text
     author = quote_soup.select_one(".author").text
     tags_soup = quote_soup.select(".tags")
     tags = [tag.text.replace("Tags:", "").split() for tag in tags_soup][0]
+
+    if author not in all_authors:
+        all_authors[author] = parse_single_author(author)
 
     return Quote(text=text, author=author, tags=tags)
 
@@ -43,12 +83,29 @@ def get_all_quote() -> list[Quote]:
     return all_quote
 
 
+def get_all_authors() -> list[Author]:
+    return [
+        Author(
+            full_name=author,
+            place_of_born=all_authors[author]["place_of_born"],
+            date_of_born=all_authors[author]["date_of_born"],
+            descriptions=all_authors[author]["descriptions"]
+        ) for author in all_authors
+    ]
+
+
 def main(output_csv_path: str) -> None:
-    with open(output_csv_path, "w") as file:
-        writer = csv.writer(file)
+    with open(output_csv_path, "w") as quote_file:
+        writer = csv.writer(quote_file)
         writer.writerow(QUOTE_FIELDS)
         quotes = get_all_quote()
         writer.writerows(astuple(quote) for quote in quotes)
+
+    with open(AUTHORS_OUTPUT_CSV_PATH, "w") as author_file:
+        writer = csv.writer(author_file)
+        writer.writerow(AUTHOR_FIELDS)
+        authors = get_all_authors()
+        writer.writerows(astuple(author) for author in authors)
 
 
 if __name__ == "__main__":

581513a59db205891409969989b0481e256213e7
Marko <marko@MacBook-Pro-Marko.local>
2022-09-21 13:42:37 +0300

    commit

--- app/parse.py
+++ app/parse.py
@@ -1,5 +1,9 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 @dataclass
 class Quote:
@@ -8,9 +12,40 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def get_page_content(page_number):
+    page = requests.get(BASE_URL, params={"page": page_number}).content
+    page_content = BeautifulSoup(page, "html.parser")
+    return page_content
+
+
+def parse_single_page_quote(quote_soup: BeautifulSoup):
+    pass
+
+def get_quotes_info() -> [Quote]:
+    page_number = 1
+    all_quotes = get_page_content(page_number=page_number).select(".quote")
+    pagination = get_page_content(page_number).select(".next")
+    while pagination:
+        page_number += 1
+        print(f"parsing {page_number} page")
+        all_quotes.extend(get_page_content(page_number))
+        pagination = 0
+        pagination = get_page_content(page_number).select(".next")
+
+    print(all_quotes)
+
+
+
+
+
+
 def main(output_csv_path: str) -> None:
     pass
 
 
 if __name__ == "__main__":
-    main("quotes.csv")
+    # main("quotes.csv")
+    get_quotes_info()

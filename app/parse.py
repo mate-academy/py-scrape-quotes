9ac7f59b39ceccbb6d2a42af3a4dec314fb43a5a
Soroka Igor <copokaigor@gmail.com>
2022-12-14 10:01:22 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass, fields, astuple
+from typing import Any
+
 import requests
 from bs4 import BeautifulSoup, Tag
 import csv
@@ -18,10 +20,6 @@ class Author:
 
 
 PRODUCT_FIELDS_AUTHORS = [field.name for field in fields(Author)]
-with open("Authors.csv", "a", encoding="utf-8") as quotes:
-    writer = csv.writer(quotes)
-    writer.writerow(PRODUCT_FIELDS_AUTHORS)
-    quotes.close()
 
 
 @dataclass
@@ -32,11 +30,6 @@ class Quote:
 
 
 PRODUCT_FIELDS = [field.name for field in fields(Quote)]
-
-with open("quotes.csv", "a", encoding="utf-8") as quotes:
-    writer = csv.writer(quotes)
-    writer.writerow(PRODUCT_FIELDS)
-    quotes.close()
 
 
 def parse_single_quote(soup: Tag) -> Quote:
@@ -58,21 +51,20 @@ def find_authors_url(soup: BeautifulSoup) -> list:
         author_url = i.get("href")
         if author_url not in Author.lib:
             new_list.append(author_url)
-    print(f"total authors in library {len(new_list)}")
     return new_list
 
 
-def parse_authors(url: str) -> Author:
-    print("start authors")
+def parse_authors(url: str) -> [Author]:
     request = requests.get(url)
     soup_authors = BeautifulSoup(request.content, "html.parser")
     domain = find_authors_url(soup_authors)
+    list_authors = []
     for author in domain:
         link = urljoin(url, author)
         request = requests.get(link)
         soup_authors = BeautifulSoup(request.content, "html.parser")
         author_info = soup_authors.select_one(".author-details")
-        return Author(
+        list_authors.append(Author(
             name=soup_authors.select_one(
                 "h3.author-title").text.split("\n")[0],
             born_date=author_info.select_one(
@@ -81,40 +73,46 @@ def parse_authors(url: str) -> Author:
                 "span.author-born-location").text,
             biography=author_info.select_one(
                 "div.author-description"
-            ).text)
+            ).text))
+    return list_authors
 
 
-def main(output_csv_path: str, url: str = None) -> None:
+def main(output_csv_path: str) -> None:
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as quote:
+        writer = csv.writer(quote)
+        writer.writerow(PRODUCT_FIELDS)
+    with open("Authors.csv", "w", encoding="utf-8", newline="") as author:
+        writer = csv.writer(author)
+        writer.writerow(PRODUCT_FIELDS_AUTHORS)
+    new_func_to_pass_tests(output_csv_path, MAIN_PAGE)
+
+
+def new_func_to_pass_tests(output_csv_path: str, url: str = None) -> Any:
     request_for_page = requests.get(url)
     soup = BeautifulSoup(request_for_page.content, "html.parser")
     need_to_parse = soup.select(".col-md-8 .quote")
     all_quote = [parse_single_quote(quote) for quote in need_to_parse]
     save_to_csv(output_csv_path, all_quote)
-    print(f"Parse {url} and takes {len(all_quote)} new quotes")
-    all_authors = [parse_authors(url)]
+    all_authors = parse_authors(url)
     save_authors_to_csv("Authors.csv", all_authors)
     try:
-        find_another_page(soup)
-        new_page = find_another_page(soup)
-        page = urljoin(MAIN_PAGE, new_page)
-        return main(output_csv_path, page)
-    except TypeError:
+        page = urljoin(MAIN_PAGE, find_another_page(soup))
+        return new_func_to_pass_tests(output_csv_path, page)
+    except AttributeError:
         print("no more pages")
 
 
 def save_to_csv(output_csv_path: str, all_quote: [Quote]) -> None:
-    with open(output_csv_path, "a", encoding="utf-8") as quote:
-        write_quo = csv.writer(quote)
-        write_quo.writerows([astuple(x) for x in all_quote])
-        quotes.close()
+    with open(output_csv_path, "a", encoding="utf-8", newline="") as quote:
+        writer = csv.writer(quote)
+        writer.writerows([astuple(x) for x in all_quote])
 
 
 def save_authors_to_csv(output_csv_path: str, all_authors: [Author]) -> None:
-    with open(output_csv_path, "a", encoding="utf-8") as author:
-        writer_author = csv.writer(author)
-        writer_author.writerows([astuple(author) for author in all_authors])
-        quotes.close()
+    with open(output_csv_path, "a", encoding="utf-8", newline="") as author:
+        writer = csv.writer(author)
+        writer.writerows([astuple(author) for author in all_authors])
 
 
 if __name__ == "__main__":
-    print(main("quotes.csv", MAIN_PAGE))
+    main("quotes.csv")

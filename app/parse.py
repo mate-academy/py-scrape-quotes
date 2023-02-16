31204fb2cf793913bbe40d59cca3a128fb98781b
vadim-kosnovskiy <kvmltkv@gmail.com>
2023-02-16 19:12:08 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,16 +1,75 @@
-from dataclasses import dataclass
+import csv
+import requests_cache
+
+from dataclasses import dataclass, fields, astuple
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+session = requests_cache.CachedSession('demo_cache')
+
+
+BASE_URL = "https://quotes.toscrape.com/"
+QUOTES_OUTPUT_CSV_PATH = "quotes.csv"
 
 
 @dataclass
 class Quote:
     text: str
     author: str
+    about_author: str
     tags: list[str]
 
 
-def main(output_csv_path: str) -> None:
-    pass
+QUOTES_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote_soup: BeautifulSoup, home_url, session) -> Quote:
+    url_about_author = urljoin(home_url, quote_soup.select_one("span > a")["href"])
+    page = session.get(url_about_author).content
+    soup = BeautifulSoup(page, "html.parser")
+    description = soup.select_one("div.author-description").text
+    quote = Quote(
+        text=quote_soup.select_one("span.text").text,
+        author=quote_soup.select_one("span > small.author").text,
+        about_author=description.lstrip(),
+        tags=[tag_soup.text for tag_soup in quote_soup.select("div.tags > a.tag")]
+    )
+    return quote
+
+
+def get_home_quotes():
+    session = requests_cache.CachedSession(BASE_URL)
+    page_first = session.get(BASE_URL).content
+    soup = BeautifulSoup(page_first, "html.parser")
+    quotes = soup.select(".quote")
+    all_quotes = [parse_single_quote(quote_soup, BASE_URL, session) for quote_soup in quotes]
+    url_next_page = soup.select_one("nav > ul > li.next > a")["href"]
+    while url_next_page:
+        home_url = urljoin(BASE_URL, url_next_page)
+        page = session.get(home_url).content
+        soup = BeautifulSoup(page, "html.parser")
+        quotes = soup.select(".quote")
+        if soup.select_one("nav > ul > li.next"):
+            url_next_page = soup.select_one("nav > ul > li.next > a")["href"]
+        else:
+            url_next_page = 0
+
+        all_quotes.extend([parse_single_quote(quote_soup, home_url, session) for quote_soup in quotes])
+
+    return all_quotes
+
+
+def write_qoutes_to_csv(quotes: [Quote]):
+    with open(QUOTES_OUTPUT_CSV_PATH, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTES_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
+
+
+def main() -> None:
+    all_quotes = get_home_quotes()
+    write_qoutes_to_csv(all_quotes)
 
 
 if __name__ == "__main__":
-    main("quotes.csv")
+    main()

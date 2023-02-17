72bc3bbde47d8c5faeca6e90faef38716693b072
vadim-kosnovskiy <kvmltkv@gmail.com>
2023-02-17 22:37:43 +0200

    Renamed variables

--- app/parse.py
+++ app/parse.py
@@ -32,23 +32,23 @@ def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
 
 
 def get_home_quotes() -> [Quote]:
-    url_page_first = requests.get(BASE_URL).content
-    soup = BeautifulSoup(url_page_first, "html.parser")
+    page_first = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page_first, "html.parser")
     quotes = soup.select(".quote")
-    next_page = soup.select_one("nav > ul > li.next > a")["href"]
+    next_page_name = soup.select_one("nav > ul > li.next > a")["href"]
     all_quotes = [
         parse_single_quote(
             quote_soup) for quote_soup in quotes
     ]
-    while next_page:
-        url_next_page = urljoin(BASE_URL, next_page)
+    while next_page_name:
+        url_next_page = urljoin(BASE_URL, next_page_name)
         page = requests.get(url_next_page).content
         soup = BeautifulSoup(page, "html.parser")
         quotes = soup.select(".quote")
         if soup.select_one("nav > ul > li.next"):
-            next_page = soup.select_one("nav > ul > li.next > a")["href"]
+            next_page_name = soup.select_one("nav > ul > li.next > a")["href"]
         else:
-            next_page = 0
+            next_page_name = 0
 
         all_quotes.extend([
             parse_single_quote(

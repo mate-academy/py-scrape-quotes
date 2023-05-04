4a9f2aade2dedbd9a9e9e9b65c40d61e8b77cc4d
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-05-04 19:48:20 +0300

    add get_single_page_quotes function

--- app/parse.py
+++ app/parse.py
@@ -4,7 +4,7 @@ import requests
 from dataclasses import dataclass
 from bs4 import BeautifulSoup
 
-PAGES_URL = "https://quotes.toscrape.com/page/"
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -14,34 +14,42 @@ class Quote:
     tags: list[str]
 
 
-def get_quotes() -> list[Quote]:
+def get_single_page_quotes(page_soup):
     quotes_list = []
+    quotes = page_soup.select(".quote")
 
-    page_num = 1
-    quotes = BeautifulSoup(
-        requests.get(f"{PAGES_URL}{page_num}/").content,
-        "html.parser"
-    ).select(".quote")
-
-    while quotes:
-        for quote in quotes:
-            quotes_list.append(Quote(
-                text=quote.select_one(".text").text,
-                author=quote.select_one(".author").text,
-                tags=[tag.text for tag in quote.select(".tag")]
-            ))
-
-        page_num += 1
-        quotes = BeautifulSoup(
-            requests.get(f"{PAGES_URL}{page_num}/").content,
-            "html.parser"
-        ).select(".quote")
+    for quote in quotes:
+        quotes_list.append(Quote(
+            text=quote.select_one(".text").text,
+            author=quote.select_one(".author").text,
+            tags=[tag.text for tag in quote.select(".tag")]
+        ))
 
     return quotes_list
 
 
-def main(output_csv_path: str) -> None:
+def get_quotes() -> list[Quote]:
+    all_quotes_list = []
 
+    with requests.Session() as session:
+        url = BASE_URL
+        url_list = [url]
+
+        while True:
+            response = session.get(url)
+            page_soup = BeautifulSoup(response.content, "html.parser")
+            all_quotes_list.extend(get_single_page_quotes(page_soup))
+            next_page = page_soup.select_one(".next > a")
+            url_list.append(next_page)
+            try:
+                url = f"{BASE_URL}{next_page['href']}"
+            except TypeError:
+                break
+
+    return all_quotes_list
+
+
+def main(output_csv_path: str) -> None:
     with open(output_csv_path, "w", newline="") as csv_quotes:
         headers = ["text", "author", "tags"]
         writer = csv.writer(csv_quotes)

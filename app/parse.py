6debb7e8fddb1cc00c9616056c678dfcb02b5cfd
Roman Senyshyn <senjaskorp@gmail.com>
2023-05-04 18:41:01 +0300

    1 commit

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
+import csv
+import time
 from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "http://quotes.toscrape.com"
 
 
 @dataclass
@@ -8,9 +15,47 @@ class Quote:
     tags: list[str]
 
 
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    text = quote_soup.select_one(".text").text
+    author = quote_soup.select_one(".author").text
+    tags = [tag.text for tag in quote_soup.select(".tag")]
+
+    return Quote(text, author, tags)
+
+
+def parse_single_page_quotes(page_soup: BeautifulSoup) -> list[Quote]:
+    quotes = page_soup.select(".quote")
+    return [parse_single_quote(quote) for quote in quotes]
+
+
+def get_all_quotes() -> list[Quote]:
+    req = requests.get(URL).content
+    first_page_soup = BeautifulSoup(req, "html.parser")
+    all_quotes = parse_single_page_quotes(first_page_soup)
+    next_page = first_page_soup.select_one(".next > a")
+    with requests.Session() as session:
+        while next_page:
+            response = session.get(f'{URL}{next_page.attrs["href"]}')
+            page_soup = BeautifulSoup(response.content, "html.parser")
+            all_quotes.extend(parse_single_page_quotes(page_soup))
+            next_page = page_soup.select_one(".next > a")
+
+    return all_quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_quotes()
+
+    with open(output_csv_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(Quote.__dataclass_fields__.keys())
+        writer.writerows(
+            [(quote.text, quote.author, quote.tags) for quote in quotes]
+        )
 
 
 if __name__ == "__main__":
+    start = time.perf_counter()
     main("quotes.csv")
+    end = time.perf_counter()
+    print("Elapsed:", end - start)

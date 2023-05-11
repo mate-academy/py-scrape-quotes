a51fcd4de1323299d042815f4f8fa2a7fdd14724
Oleksandr Leha <legus96@gmail.com>
2023-05-11 20:54:41 +0300

    Solution_with_changes

--- app/parse.py
+++ app/parse.py
@@ -4,6 +4,8 @@ from urllib.parse import urljoin
 import csv
 from dataclasses import dataclass
 
+URL = "https://quotes.toscrape.com/"
+
 
 @dataclass
 class Quote:
@@ -13,15 +15,20 @@ class Quote:
     author_bio: str = ""
 
 
+def create_single_quote(quote_element: BeautifulSoup) -> Quote:
+    text = quote_element.find("span", {"class": "text"}).text
+    author = quote_element.find("small", {"class": "author"}).text
+    tags = [tag.text for tag in quote_element.select(".tag")]
+    return Quote(text=text, author=author, tags=tags)
+
+
 def get_quotes(url: str) -> list:
     res = requests.get(url)
     soup = BeautifulSoup(res.text, "html.parser")
     quotes = []
-    for quote in soup.select(".quote"):
-        text = quote.find("span", {"class": "text"}).text
-        author = quote.find("small", {"class": "author"}).text
-        tags = [tag.text for tag in quote.select(".tag")]
-        quotes.append(Quote(text=text, author=author, tags=tags))
+    for quote_element in soup.select(".quote"):
+        quote = create_single_quote(quote_element)
+        quotes.append(quote)
     next_page = soup.select_one(".next a")
     if next_page:
         next_url = urljoin(url, next_page["href"])
@@ -29,9 +36,7 @@ def get_quotes(url: str) -> list:
     return quotes
 
 
-def main(output_csv_path: str) -> None:
-    url = "https://quotes.toscrape.com/"
-    quotes = get_quotes(url)
+def write_quotes_to_csv(quotes: list, output_csv_path: str) -> None:
     with open(output_csv_path, "w", newline="", encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(["text", "author", "tags"])
@@ -41,6 +46,11 @@ def main(output_csv_path: str) -> None:
                 quote.author,
                 quote.tags,
             ])
+
+
+def main(output_csv_path: str) -> None:
+    quotes = get_quotes(URL)
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":

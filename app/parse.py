a9f3a379ebfab093a55306b643aaf1b282bddd90
Іван Березянський <mamanadarin@gmail.com>
2023-05-27 14:20:16 +0300

    develop

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields
+from urllib.parse import urljoin
+
+import requests as requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +15,43 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    return (Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=[i.text for i in quote_soup.select(".tag")]
+    ))
+
+
+def parse_page(page_soup: BeautifulSoup) -> list[Quote]:
+    return [parse_single_quote(quote_soap) for quote_soap in page_soup]
+
+
+def get_all_quotes() -> list:
+    page = requests.get(BASE_URL).content
+    soup = BeautifulSoup(page, "html.parser")
+
+    i = 2
+    quotes = parse_page(soup.select(".quote"))
+
+    while soup.select_one(".next"):
+        page = requests.get(urljoin(BASE_URL, f"page/{i}/")).content
+        soup = BeautifulSoup(page, "html.parser")
+        quotes.extend(parse_page(soup.select(".quote")))
+        i += 1
+
+    return quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTE_FIELDS)
+        for quote in get_all_quotes():
+            writer.writerow([quote.text, quote.author, quote.tags])
 
 
 if __name__ == "__main__":

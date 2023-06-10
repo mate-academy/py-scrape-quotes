5244b0b26020904bfaabef486f85cba1fd07cf8c
AlenOl <stoveprofi@gmail.com>
2023-06-10 14:17:54 +0100

    Implemented main.py function to write the results of spider's scanning

--- app/parse.py
+++ app/parse.py
@@ -1,3 +1,6 @@
+import scrapy
+from scrapy.http import Response
+from scrapy.crawler import CrawlerProcess
 from dataclasses import dataclass
 
 
@@ -8,8 +11,34 @@ class Quote:
     tags: list[str]
 
 
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'https://quotes.toscrape.com/page/1/',
+    ]
+
+    def parse(self, response: Response, **kwargs):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    process = CrawlerProcess(settings={
+        "FEEDS": {
+            output_csv_path: {"format": "csv"},
+        },
+    })
+
+    process.crawl(QuotesSpider)
+    process.start()
 
 
 if __name__ == "__main__":

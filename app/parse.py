b1833b6a6fe635807b563b15af29e38a84102b22
graandda <graanddadays@gmail.com>
2023-06-23 17:12:37 +0300

    fix flake8 issues

--- app/parse.py
+++ app/parse.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 from dataclasses import dataclass
 
 
-
 @dataclass
 class Quote:
     text: str
@@ -20,33 +19,36 @@ class Parse:
     SOUP = ""
     DO_PAGINATION = True
 
-    def make_request(self):
+    def make_request(self) -> None:
         if self.PARAMS:
-            r = requests.get(self.HOME_PAGE + self.PARAMS)
+            re = requests.get(self.HOME_PAGE + self.PARAMS)
         else:
-            r = requests.get(self.HOME_PAGE)
-        self.SOUP = BeautifulSoup(r.text, 'html.parser')
+            re = requests.get(self.HOME_PAGE)
+        self.SOUP = BeautifulSoup(re.text, "html.parser")
 
-    def parse_page(self):
+    def parse_page(self) -> None:
         for el in self.SOUP.find_all(class_="quote"):
-            self.RESULT_TABLE.append(Quote(
-                text=el.find(itemprop="text").text,
-                author=el.find(itemprop="author").text,
-                tags=[] if el.find(itemprop="keywords")['content'].split(',') == [''] else el.find(itemprop="keywords")['content'].split(',')
-                  ))
+            list_of_cont = el.find(itemprop="keywords")["content"].split(",")
+            self.RESULT_TABLE.append(
+                Quote(
+                    text=el.find(itemprop="text").text,
+                    author=el.find(itemprop="author").text,
+                    tags=[] if list_of_cont == [""]
+                    else list_of_cont,
+                )
+            )
 
         if self.SOUP.find(class_="next"):
-            self.PARAMS = str(self.SOUP.find(class_="next").a['href'])
+            self.PARAMS = str(self.SOUP.find(class_="next").a["href"])
             self.DO_PAGINATION = True
         else:
             self.DO_PAGINATION = False
 
-    def fill_table(self, output_csv_path):
+    def fill_table(self, output_csv_path: str) -> None:
         with open(output_csv_path, "w") as stream:
             writer = csv.writer(stream)
-            writer.writerow(['text', 'author', 'tags'])
+            writer.writerow(["text", "author", "tags"])
             for row in self.RESULT_TABLE:
-                print(row.tags)
                 writer.writerow([row.text, row.author, row.tags])
 
 
@@ -60,4 +62,3 @@ def main(output_csv_path: str) -> None:
 
 if __name__ == "__main__":
     main("quotes.csv")
-

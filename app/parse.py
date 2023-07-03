9e0649c98ab072aa90bfc9593265e68250dd6332
SlavikShcherbak <misternictof@gmail.com>
2023-07-03 12:31:16 +0300

    recycled using OOP

--- app/parse.py
+++ app/parse.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
 from dataclasses import dataclass, astuple
 import requests
 from bs4 import BeautifulSoup
 import csv
 from urllib.parse import urljoin
+
 
 BASE_URL = "https://quotes.toscrape.com/page/1/"
 
@@ -13,6 +15,33 @@ class Quote:
     author: str
     tags: list[str]
 
+    def parse_single_quote(self, quote_soup: BeautifulSoup) -> Quote:
+        return Quote(
+            text=quote_soup.select_one(".text").text,
+            author=quote_soup.select_one(".author").text,
+            tags=[tag.text for tag in quote_soup.select(".tags > a.tag")]
+        )
+
+    def get_product_on_page(self, quotes: BeautifulSoup) -> [Quote]:
+        quotes = quotes.select(".quote")
+        return [self.parse_single_quote(self, quote) for quote in quotes]
+
+    def get_quotes(self) -> [Quote]:
+        page = requests.get(BASE_URL).content
+        soup = BeautifulSoup(page, "html.parser")
+        next_page = self.get_next_page(self, soup)
+        quotes = self.get_product_on_page(self, soup)
+        while next_page:
+            page = requests.get(urljoin(BASE_URL, next_page["href"])).content
+            soup = BeautifulSoup(page, "html.parser")
+            quotes.extend(self.get_product_on_page(self, soup))
+            next_page = self.get_next_page(self, soup)
+
+        return quotes
+
+    def get_next_page(self, page_soup: BeautifulSoup) -> str | None:
+        return page_soup.select_one("li.next > a")
+
 
 def write_to_csv(output_csv_path: str, quotes: Quote) -> None:
     with open(output_csv_path, "w", encoding="utf-8", newline="") as file:
@@ -21,39 +50,8 @@ def write_to_csv(output_csv_path: str, quotes: Quote) -> None:
         writer.writerows([astuple(quote) for quote in quotes])
 
 
-def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
-    return Quote(
-        text=quote_soup.select_one(".text").text,
-        author=quote_soup.select_one(".author").text,
-        tags=[tag.text for tag in quote_soup.select(".tags > a.tag")]
-    )
-
-
-def get_product_on_page(quotes: BeautifulSoup) -> [Quote]:
-    quotes = quotes.select(".quote")
-    return [parse_single_quote(quote) for quote in quotes]
-
-
-def get_next_page(page_soup: BeautifulSoup) -> str | None:
-    return page_soup.select_one("li.next > a")
-
-
-def get_quotes() -> [Quote]:
-    page = requests.get(BASE_URL).content
-    soup = BeautifulSoup(page, "html.parser")
-    next_page = get_next_page(soup)
-    quotes = get_product_on_page(soup)
-    while next_page:
-        page = requests.get(urljoin(BASE_URL, next_page["href"])).content
-        soup = BeautifulSoup(page, "html.parser")
-        quotes.extend(get_product_on_page(soup))
-        next_page = get_next_page(soup)
-
-    return quotes
-
-
 def main(output_csv_path: str) -> None:
-    quotes = get_quotes()
+    quotes = Quote.get_quotes(Quote)
     write_to_csv(output_csv_path, quotes)
 
 

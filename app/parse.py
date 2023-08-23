32e48a86db843c9f60755219944971ace7afee7f
Dmytry Bondariev <bondarievmd@gmail.com>
2023-08-23 17:58:45 +0300

    Solution2

--- app/parse.py
+++ app/parse.py
@@ -16,7 +16,7 @@ class Quote:
 
 def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
     return Quote(
-        text=quote_soup.select_one(".text").text.strip("“”"),
+        text=quote_soup.select_one(".text").text,
         author=quote_soup.select_one(".author").text,
         tags=[tag.text for tag in quote_soup.select(".tags > a")],
     )

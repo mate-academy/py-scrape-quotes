64428e82c264e9ce01939fb22d18042a41ee0b77
Tarasidze <dtar84@ukr.net>
2023-08-29 14:35:35 +0300

    fix

--- app/parse.py
+++ app/parse.py
@@ -1,10 +1,10 @@
 from app.description import BASE_URL, EXT_URL_QUOTE, QUOTES_FILE, AUTHORS_FILE
 
 from app.quote_parse_logic import get_quote_from_all_pages, write_quote_to_file
-from app.author_parse_logic import (
-    get_authors_from_all_pages,
-    write_author_to_file
-)
+# from app.author_parse_logic import (
+#     get_authors_from_all_pages,
+#     write_author_to_file
+# )
 
 
 def main(output_csv_quote_path: str) -> None:

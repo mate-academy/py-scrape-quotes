a1a569ae2c33e8669a634fafbce8b2586dc24144
Eugene Polunin <altahoma@gmail.com>
2023-08-30 17:31:24 +0300

    Implemented quote parsing

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup, Tag
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +15,52 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_singe_quote(quote_soup: Tag) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=[tag.text for tag in quote_soup.select(".tag")],
+    )
+
+
+def get_single_page_quotes(page_soup: BeautifulSoup) -> [Quote]:
+    quotes = page_soup.select(".quote")
+
+    return [parse_singe_quote(quote_soup=quote_soup) for quote_soup in quotes]
+
+
+def get_quotes() -> [Quote]:
+    all_quotes = []
+    page_number = 1
+
+    while True:
+        print(f"Parsing page number {page_number}...")
+        url = urljoin(BASE_URL, f"page/{page_number}/")
+        page = requests.get(url).content
+        soup = BeautifulSoup(page, "html.parser")
+
+        if "No quotes found!" in soup.text:
+            break
+
+        all_quotes.extend(get_single_page_quotes(page_soup=soup))
+        page_number += 1
+
+    return all_quotes
+
+
+def write_quotes_to_csv(quotes: [Quote], path: str) -> None:
+    with open(path, "w") as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows(astuple(quote) for quote in quotes)
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_quotes()
+    write_quotes_to_csv(quotes=quotes, path=output_csv_path)
 
 
 if __name__ == "__main__":

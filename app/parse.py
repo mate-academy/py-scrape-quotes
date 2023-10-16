c1b83da002fc23ca1a4094285a450f9b00108ad8
BohdanFSD-temp <luitko009@gmail.com>
2023-10-16 17:21:01 +0000

    Fixing decoding issue

--- app/parse.py
+++ app/parse.py
@@ -1,10 +1,11 @@
-import csv
 from dataclasses import dataclass, fields, astuple
-from typing import Optional
-from urllib.parse import urljoin
+from typing import Union
+import csv
 
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -12,35 +13,28 @@ class Quote:
     text: str
     author: str
     tags: list[str]
-    biography: str
 
 
-BASE_URL = "https://quotes.toscrape.com"
 QUOTE_FIELDS = [field.name for field in fields(Quote)]
-AUTHOR_BIO_CACHE = {}
 
 
 def get_page_html(url: str) -> BeautifulSoup:
-    response = requests.get(url).content
-    return BeautifulSoup(response, "html.parser")
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+    try:
+        return BeautifulSoup(response.text, "html.parser")
+    except UnicodeDecodeError:
+        print("Error parsing the following content from URL:", url)
+        print(response.text)  # This will print the content that's causing the issue
+        raise
 
 
-def get_author_bio(author_url: str) -> str:
-    if author_url in AUTHOR_BIO_CACHE:
-        return AUTHOR_BIO_CACHE[author_url]
-
-    soup = get_page_html(author_url)
-    bio = soup.select_one(".author-description").text.strip()
-    AUTHOR_BIO_CACHE[author_url] = bio
-    return bio
-
-
-def get_path_to_next_page(url: str) -> Optional[str]:
+def get_path_to_next_page(url: str) -> Union[str, bool]:
     link_to_next_page = get_page_html(url).select_one(
         ".pager > .next")
     if link_to_next_page:
         return str(link_to_next_page.find_next("a")["href"])[1:]
-    return None
+    return False
 
 
 def get_quote_from_single_page(url: str) -> list[Quote]:
@@ -53,20 +47,11 @@ def get_quote_from_single_page(url: str) -> list[Quote]:
     quotes_tags = [
         [tag.text for tag in text.select(".tag")] for text in quotes_div
     ]
-    quotes_bio = []
-    for quote_soup in quotes_div:
-        author_url = urljoin(
-            BASE_URL,
-            quote_soup.select_one(".author + a")["href"]
-        )
-        quotes_bio.append(get_author_bio(author_url))
-
     return [
         Quote(
             text=quotes_text[index],
             author=quotes_author[index],
-            tags=quotes_tags[index],
-            biography=quotes_bio[index]
+            tags=quotes_tags[index]
         ) for index in range(len(quotes_div))
     ]
 
@@ -86,7 +71,8 @@ def get_all_quotes(url: str) -> list[Quote]:
 
 
 def main(output_csv_path: str) -> None:
-    with open(output_csv_path, "w") as file:
+    pass
+    with open(output_csv_path, "w", encoding="utf-8") as file:
         writer = csv.writer(file)
         quotes = get_all_quotes(BASE_URL)
         writer.writerow(QUOTE_FIELDS)

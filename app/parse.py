c2244f557dd545b1676d86ee167b2b323821aa54
BohdanFSD-temp <luitko009@gmail.com>
2023-10-17 19:19:10 +0000

    Refactored solution

--- app/parse.py
+++ app/parse.py
@@ -18,63 +18,59 @@ class Quote:
 QUOTE_FIELDS = [field.name for field in fields(Quote)]
 
 
-def get_page_html(url: str) -> BeautifulSoup:
-    response = requests.get(url)
-    response.encoding = "utf-8"
-    try:
-        return BeautifulSoup(response.text, "html.parser")
-    except UnicodeDecodeError:
-        print("Error parsing the following content from URL:", url)
-        print(response.text)
-        raise
+class QuoteParser:
+    def __init__(self, base_url: str) -> None:
+        self.base_url = base_url
 
+    @staticmethod
+    def get_page_html(url: str) -> BeautifulSoup:
+        response = requests.get(url)
+        response.encoding = "utf-8"
+        try:
+            return BeautifulSoup(response.text, "html.parser")
+        except UnicodeDecodeError:
+            print("Error parsing the following content from URL:", url)
+            print(response.text)
+            raise
 
-def get_path_to_next_page(url: str) -> Union[str, bool]:
-    link_to_next_page = get_page_html(url).select_one(
-        ".pager > .next")
-    if link_to_next_page:
-        return str(link_to_next_page.find_next("a")["href"])[1:]
-    return False
+    def get_path_to_next_page(self, url: str) -> Union[str, bool]:
+        link_to_next_page = (
+            QuoteParser.get_page_html(url).select_one(".pager > .next"))
+        if link_to_next_page:
+            return str(link_to_next_page.find_next("a")["href"])[1:]
+        return False
 
+    def get_quote_from_single_page(self, url: str) -> list[Quote]:
+        quotes_div = QuoteParser.get_page_html(url).select(".quote")
+        quotes_text = \
+            [text.select_one(".text").text for text in quotes_div]
+        quotes_author = \
+            [text.select_one(".author").text for text in quotes_div]
+        quotes_tags = \
+            [[tag.text for tag in text.select(".tag")] for text in quotes_div]
+        return [
+            Quote(text=quotes_text[index],
+                  author=quotes_author[index],
+                  tags=quotes_tags[index])
+            for index in range(len(quotes_div))
+        ]
 
-def get_quote_from_single_page(url: str) -> list[Quote]:
-    current_url = url
-    quotes_div = get_page_html(current_url).select(".quote")
-    quotes_text = [text.select_one(".text").text for text in quotes_div]
-    quotes_author = [
-        text.select_one(".author").text for text in quotes_div
-    ]
-    quotes_tags = [
-        [tag.text for tag in text.select(".tag")] for text in quotes_div
-    ]
-    return [
-        Quote(
-            text=quotes_text[index],
-            author=quotes_author[index],
-            tags=quotes_tags[index]
-        ) for index in range(len(quotes_div))
-    ]
-
-
-def get_all_quotes(url: str) -> list[Quote]:
-    current_url = url
-
-    quotes = get_quote_from_single_page(current_url)
-    page = 1
-
-    while get_path_to_next_page(current_url):
-        page += 1
-        current_url = url + f"page/{page}/"
-        quotes += get_quote_from_single_page(current_url)
-
-    return quotes
+    def get_all_quotes(self) -> list[Quote]:
+        current_url = self.base_url
+        quotes = self.get_quote_from_single_page(current_url)
+        page = 1
+        while self.get_path_to_next_page(current_url):
+            page += 1
+            current_url = self.base_url + f"page/{page}/"
+            quotes += self.get_quote_from_single_page(current_url)
+        return quotes
 
 
 def main(output_csv_path: str) -> None:
-    pass
+    parser = QuoteParser(BASE_URL)
+    quotes = parser.get_all_quotes()
     with open(output_csv_path, "w", encoding="utf-8") as file:
         writer = csv.writer(file)
-        quotes = get_all_quotes(BASE_URL)
         writer.writerow(QUOTE_FIELDS)
         writer.writerows(astuple(quote) for quote in quotes)
 

15e7af8838c5aa6aeb6790a2cc7b845525ecbdf4
OlegatorLE <oleh.travin@gmail.com>
2023-11-06 16:37:24 +0200

    Wrote in .csv file home page

--- app/parse.py
+++ app/parse.py
@@ -1,7 +1,9 @@
+import csv
+
 import requests
 from bs4 import BeautifulSoup
 
-from dataclasses import dataclass
+from dataclasses import dataclass, fields, astuple
 
 BASE_URL = "https://quotes.toscrape.com/"
 
@@ -13,12 +15,15 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
 def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
-    print(dict(
-        quote=quote_soup.select_one(".text").text,
+    return Quote(
+        text=quote_soup.select_one(".text").text,
         author=quote_soup.select_one(".author").text,
         tags=quote_soup.select(".keywords")[0]["content"].split(",")
-    ))
+    )
 
 
 def get_home_quotes() -> [Quote]:
@@ -29,13 +34,13 @@ def get_home_quotes() -> [Quote]:
     return [parse_single_quote(quote_soup) for quote_soup in quotes]
 
 
-def main():
-    get_home_quotes()
-
-# def main(output_csv_path: str) -> None:
-#     get_home_quotes()
+def main(output_csv_path: str) -> None:
+    quotes = get_home_quotes()
+    with open(output_csv_path, "w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
 
 
 if __name__ == "__main__":
-    main()
-    # main("quotes.csv")
+    main("quotes.csv")

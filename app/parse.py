05d0430fcddae8e2e1e8a8781f615a1ac4aafd2d
OlegatorLE <oleh.travin@gmail.com>
2023-11-06 17:08:24 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -22,20 +22,33 @@ def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
     return Quote(
         text=quote_soup.select_one(".text").text,
         author=quote_soup.select_one(".author").text,
-        tags=quote_soup.select(".keywords")[0]["content"].split(",")
+        tags=[tag.text for tag in quote_soup.select(".tag")]
     )
 
 
-def get_home_quotes() -> [Quote]:
-    page = requests.get(BASE_URL).content
-    soup = BeautifulSoup(page, "html.parser")
-    quotes = soup.select(".quote")
-
+def get_single_page_quotes(page_soup: BeautifulSoup) -> [Quote]:
+    quotes = page_soup.select(".quote")
     return [parse_single_quote(quote_soup) for quote_soup in quotes]
 
 
+def get_all_quotes() -> [Quote]:
+    quotes = []
+    page_num = 1
+
+    while True:
+        page = requests.get(f"{BASE_URL}/page/{page_num}").content
+        soup = BeautifulSoup(page, "html.parser")
+        all_quotes_from_page = get_single_page_quotes(soup)
+        if not all_quotes_from_page:
+            break
+        quotes.extend(all_quotes_from_page)
+        page_num += 1
+
+    return quotes
+
+
 def main(output_csv_path: str) -> None:
-    quotes = get_home_quotes()
+    quotes = get_all_quotes()
     with open(output_csv_path, "w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(QUOTE_FIELDS)

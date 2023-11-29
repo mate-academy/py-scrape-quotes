a96405fd5b254df98d53d93e969d53cf64e88dfc
Dimo-n <dimon.chmara@gmail.com>
2023-11-29 11:06:56 +0200

    Create parse_single_quote,  get_single_page_quotes and get_quotes functions

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
+import csv
+import requests
+
 from dataclasses import dataclass
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://quotes.toscrape.com/"
 
 
 @dataclass
@@ -8,8 +15,29 @@ class Quote:
     tags: list[str]
 
 
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=quote_soup.select_one(".keywords")["content"].split(",")
+    )
+
+
+def get_single_page_quotes(page_soup: BeautifulSoup) -> [Quote]:
+    quotes = page_soup.select(".quote")
+
+    return [parse_single_quote(quote_soup) for quote_soup in quotes]
+
+
+def get_quotes() -> [Quote]:
+    first_page = requests.get(BASE_URL).content
+    first_page_soup = BeautifulSoup(first_page, "html.parser")
+    quotes = get_single_page_quotes(first_page_soup)
+    print(quotes)
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    get_quotes()
 
 
 if __name__ == "__main__":

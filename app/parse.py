b00dcc3c2476225dd3b8eb9db9f4761e22f6df8b
Denys <den.prislipskyi@gmail.com>
2023-12-27 20:03:48 +0100

    Scrape

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,11 @@
 from dataclasses import dataclass
+import csv
+
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://quotes.toscrape.com"
 
 
 @dataclass
@@ -9,7 +16,46 @@ class Quote:
 
 
 def main(output_csv_path: str) -> None:
-    pass
+    all_quotes = get_all_quotes()
+    write_quotes_to_csv(output_csv_path, all_quotes)
+
+
+def get_all_quotes() -> list:
+    all_quotes = []
+    page_number = 1
+    while True:
+        url = f"{URL}/page/{page_number}/"
+        quotes = get_quotes(url)
+        if not quotes:
+            break
+        all_quotes.extend(quotes)
+        page_number += 1
+    return all_quotes
+
+
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    tags = quote_soup.select(".tag")
+    tag_texts = [tag.text for tag in tags]
+    return Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=tag_texts
+    )
+
+
+def get_quotes(url: str) -> list:
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, "html.parser")
+    quotes = soup.select(".quote")
+    return [parse_single_quote(quote_soup) for quote_soup in quotes]
+
+
+def write_quotes_to_csv(output_csv_path: str, quotes: BeautifulSoup) -> None:
+    with open(output_csv_path, mode="w", encoding="utf-8", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["text", "author", "tags"])
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, quote.tags])
 
 
 if __name__ == "__main__":

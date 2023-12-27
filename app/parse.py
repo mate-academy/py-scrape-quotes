e66f8127832ef31e7c65c0aeb749b142d2a26121
D1st3f <dima.purig@gmail.com>
2023-12-27 18:02:44 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,9 @@
+import csv
+import re
 from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup, element
 
 
 @dataclass
@@ -8,8 +13,47 @@ class Quote:
     tags: list[str]
 
 
+BASE_URL = "https://quotes.toscrape.com/"
+
+
+def _parse_single_quote(quote: element.Tag) -> Quote:
+    return Quote(
+        text=re.sub("[“”]", "",
+                    str(quote.select_one("span.text").text)),
+        author=quote.select_one("span > small.author").text,
+        tags=[tag.text for tag in quote.select("div.tags > a.tag")],
+    )
+
+
+def _get_single_page(page: bytes) -> list[Quote]:
+    soup = BeautifulSoup(page, "html.parser")
+    quotes = soup.select("div.quote")
+    return [_parse_single_quote(quote) for quote in quotes]
+
+
+def get_all_pages() -> list[Quote]:
+    page_number = 1
+    all_page_quotes = []
+    while True:
+        page = requests.get(BASE_URL + f"/page/{page_number}/")
+        if page.status_code != 200:
+            break
+        page_number += 1
+        single_page_quotes = _get_single_page(page.content)
+        if not single_page_quotes:
+            break
+        all_page_quotes.extend(single_page_quotes)
+    return all_page_quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_pages()
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(["Text", "Author", "Tags"])
+        for quote in quotes:
+            csv_writer.writerow(
+                [quote.text, quote.author, ", ".join(quote.tags)])
 
 
 if __name__ == "__main__":

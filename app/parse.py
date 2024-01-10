dcbff1671d6e235f306158c9ddf4547203c6c3d5
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2024-01-10 17:55:24 +0000

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,12 @@
-from dataclasses import dataclass
+import csv
+from dataclasses import dataclass, fields, astuple
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://quotes.toscrape.com/"
+QUOTES_OUTPUT_CSV_PATH = "result.csv"
 
 
 @dataclass
@@ -8,9 +16,50 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(information_soup: BeautifulSoup) -> Quote:
+
+    return Quote(
+        text=information_soup.select_one(".text").text,
+        author=information_soup.select_one(".author").text,
+        tags=[information_soup.select_one(".keywords")["content"]]
+    )
+
+
+def get_single_page(page_soup: BeautifulSoup) -> [Quote]:
+    information = page_soup.select(".quote")
+
+    return [parse_single_quote(information_soup) for information_soup in information]
+
+
+def get_quotes() -> [Quote]:
+    first_page = requests.get(URL).content
+    first_page_soup = BeautifulSoup(first_page, "html.parser")
+    quotes = get_single_page(first_page_soup)
+    next_page = first_page_soup.select_one(".next > a")
+
+    while next_page is not None:
+        page = requests.get(urljoin(URL, next_page.get("href"))).content
+        soup = BeautifulSoup(page, "html.parser")
+
+        quotes.extend(get_single_page(soup))
+        next_page = soup.select_one(".next > a")
+    return quotes
+
+
+def save_in_csv(information: [Quote]):
+    with open(QUOTES_OUTPUT_CSV_PATH, "w", encoding="utf-8", newline="") as file:
+        write = csv.writer(file)
+        write.writerow(QUOTE_FIELDS)
+        write.writerows([astuple(inform) for inform in information])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    information = get_quotes()
+    save_in_csv(information)
 
 
 if __name__ == "__main__":
-    main("quotes.csv")
+    main(QUOTES_OUTPUT_CSV_PATH)

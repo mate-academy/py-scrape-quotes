836afb9f945eb17a18b874e0fb1ebe5bdd796fc6
Andii Rashevskyi <phoenixerazer@gmail.com>
2024-02-07 23:59:23 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -13,9 +13,8 @@ class Quote:
     author: str
     tags: list[str]
 
-
     @classmethod
-    def parse_single_quote(cls, quote_soup: BeautifulSoup) -> 'Quote':
+    def parse_single_quote(cls, quote_soup: BeautifulSoup) -> "Quote":
         return cls(
             text=quote_soup.select_one(".text").text,
             author=quote_soup.select_one(".author").text,

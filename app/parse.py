40085627f448afc0622aa1ad571580603dea638b
nsdNite <nsd7night@gmail.com>
2024-03-02 21:23:59 +0200

    add author parsing

--- app/parse.py
+++ app/parse.py
@@ -35,6 +35,9 @@ class Author:
     description: str
 
 
+CSV_FIELDS_AUTHOR = [field.name for field in fields(Author)]
+
+
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)8s]: %(message)s",
@@ -43,6 +46,47 @@ logging.basicConfig(
         logging.StreamHandler(sys.stdout),
     ],
 )
+
+
+def get_author_urls(quote_soup: BeautifulSoup) -> str:
+
+    return quote_soup.select_one("a")["href"]
+
+
+def get_single_page_authors(page_soup: BeautifulSoup) -> list:
+    quotes = page_soup.select(".quote")
+
+    return [get_author_urls(page_soup) for page_soup in quotes]
+
+
+def parse_single_author(author_soup: BeautifulSoup) -> Author:
+
+    return Author(
+        name=author_soup.select_one(".author-title").text,
+        born_date=author_soup.select_one(".author-born-date").text,
+        born_place=author_soup.select_one(".author-born-location").text[3:],
+        description=author_soup.select_one(".author-description").text.strip(),
+    )
+
+
+def get_authors_info(urls: set) -> [Author]:
+    logging.info("Start of authors parsing")
+
+    all_authors = []
+
+    for url in urls:
+        time.sleep(random.randint(1, 3))
+
+        page_url = urljoin(HOME_URL, url)
+        logging.info(f"Parsing info from {page_url}")
+        page = requests.get(page_url).content
+        page_soup = BeautifulSoup(page, "html.parser")
+
+        all_authors.append(parse_single_author(page_soup))
+
+    logging.info(f"Collected info about {len(all_authors)} authors")
+
+    return all_authors
 
 
 def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
@@ -64,6 +108,7 @@ def get_quotes() -> [Quote]:
     logging.info("Start of quotes parsing")
 
     all_quotes = []
+    all_authors_urls = []
 
     page_num = 1
     current_page_url = urljoin(PAGE_URL, str(page_num))
@@ -76,35 +121,40 @@ def get_quotes() -> [Quote]:
         logging.info(
             f"Requesting page {current_page_url} : status: {requests.get(current_page_url).status_code}"
         )
-        time.sleep(random.randint(1, 10))
+        time.sleep(random.randint(1, 5))
 
         page = requests.get(current_page_url).content
         page_soup = BeautifulSoup(page, "html.parser")
 
         all_quotes.extend(get_single_page_quotes(page_soup))
+        all_authors_urls.extend(get_single_page_authors(page_soup))
+
         present_next = page_soup.select_one(".next")
 
         page_num += 1
         current_page_url = urljoin(PAGE_URL, str(page_num))
 
-    return all_quotes
+    return all_quotes, set(all_authors_urls)
 
 
-def output_as_csv(path, quotes: [Quote]) -> None:
+def output_as_csv(path, objs: list, csv_fields: list) -> None:
     with open(
         path,
         "w",
     ) as file:
         writer = csv.writer(file)
-        writer.writerow(CSV_FIELDS_QUOTE)
-        writer.writerows([astuple(quote) for quote in quotes])
+        writer.writerow(csv_fields)
+        writer.writerows([astuple(obj) for obj in objs])
 
-    logging.info(f"Saved {len(quotes)} quotes.")
+    logging.info(f"Saved {len(objs)} objects to {path}.")
 
 
 def main(output_csv_path: str) -> None:
-    quotes = get_quotes()
-    output_as_csv(output_csv_path, quotes)
+    results = get_quotes()
+    quotes = results[0]
+    authors = get_authors_info(results[1])
+    output_as_csv(output_csv_path, quotes, CSV_FIELDS_QUOTE)
+    output_as_csv("authors.csv", authors, CSV_FIELDS_AUTHOR)
 
 
 if __name__ == "__main__":

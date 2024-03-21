6108ad237f0b94f40a9cff3ce295f3bedbd4fce6
Lylak Ivan <lylakivanmate@gmail.com>
2024-03-21 18:33:10 +0200

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,23 @@
+import csv
+import logging
+import sys
 from dataclasses import dataclass
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = 'https://quotes.toscrape.com/'
+OUTPUT_FILE = 'quotes.csv'
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)8s]: %(message)s",
+    handlers=[
+        logging.FileHandler("perser.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
 
 
 @dataclass
@@ -8,8 +27,53 @@ class Quote:
     tags: list[str]
 
 
+def parse_single_quote(quote_soup: BeautifulSoup) -> Quote:
+    return Quote(
+        text=quote_soup.select_one(".text").text,
+        author=quote_soup.select_one(".author").text,
+        tags=[tag.text for tag in quote_soup.select(".tag")]
+    )
+
+
+def get_quotes_from_page(soup: BeautifulSoup) -> [Quote]:
+    quotes = soup.select(".quote")
+    return [parse_single_quote(quote_soup) for quote_soup in quotes]
+
+
+def get_list_quotes() -> list[Quote]:
+    quotes = []
+    page = 1
+
+    logging.info(f"Start parsing Quotes")
+
+    while True:
+        logging.info(f"Start parsing page #{page}")
+        url = f"{BASE_URL}/page/{page}/"
+        parsed_page = requests.get(url).content
+        page_soup = BeautifulSoup(parsed_page, "html.parser")
+        parsed_quotes = get_quotes_from_page(page_soup)
+
+        if not parsed_quotes:
+            break
+
+        quotes.extend(parsed_quotes)
+        page += 1
+
+    return quotes
+
+
+def write_quotes_to_csv(quotes: [Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["text", "author", "tags"])
+
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, quote.tags])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_list_quotes()
+    write_quotes_to_csv(quotes, output_csv_path)
 
 
 if __name__ == "__main__":
